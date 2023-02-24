fcb8cb54d11b6117446e51fdfcbc8d08eff364e7
Hima Fasmy <fasmi.20211062@iit.ac.lk>
2023-02-24 20:49:37 +0530

    new added
    added to fingerprint

---
+++ Fingerprint/fingerr/app/src/main/java/com/example/fingerr/SecretActivity1.java
@@ -0,0 +1,14 @@
+package com.example.fingerr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecretActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret1);
+    }
+}
