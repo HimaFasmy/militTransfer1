be1bdaf9420bba20aa6b12741bee18d4503cecf6
Pazeezz <pasindu.20200614@iit.ac.lk>
2023-03-26 21:54:38 +0530

    Created SQLite Database

---
+++ mobileSolution/LoginApplication/app/src/main/java/com/example/loginapplication/DBHelper.kt
@@ -0,0 +1,42 @@
+package com.example.loginapplication
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.core.content.contentValuesOf
+
+class DBHelper(context: Context) :SQLiteOpenHelper(context, "Userdata", null, 1) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL("create table Userdata (username TEXT primary key, password TEXT)")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL("drop table if exists Userdata")
+    }
+
+    fun insertdata(username: String, password: String): Boolean {
+        val p0= this.writableDatabase
+        val cv = ContentValues()
+        cv.put("username", username)
+        cv.put("password", password)
+        val result =p0.insert("Userdata", null, cv)
+        if (result==-1 .toLong()){
+            return false
+        }
+        return true
+    }
+    
+    fun chackuserpass(username: String, password: String): Boolean {
+        val p0 = this.writableDatabase
+        val query = "select * from Userdata where username= '$username' and password='$password'"
+        val cursor = p0.rawQuery(query, null)
+        if (cursor.count<=0){
+            cursor.close()
+            return false
+        }
+        cursor.close()
+        return true
+    }
+
+}
