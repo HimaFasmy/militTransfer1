93d1d9260b13c49c4a8e98b85c8da4edc85cad9c
Pazeezz <pasindu.20200614@iit.ac.lk>
2023-03-26 15:19:24 +0530

    Created login and signup pages

---
+++ mobileSolution/LoginApplication/app/src/main/java/com/example/loginapplication/sigup.kt
@@ -0,0 +1,11 @@
+package com.example.loginapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sigup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigup)
+    }
+}
